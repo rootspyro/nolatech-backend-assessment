2f6e5b3f117492b4f5a201a57ae953a761f8d83e
rootspyro <root.spyro@gmail.com>
2024-03-22 19:17:00 -0400

    Feat: Integrate the search of a single user

--- backend/handlers/users.cjs
+++ backend/handlers/users.cjs
@@ -8,13 +8,20 @@ async function GetUsers(req, res) {
 
   try {
 
-    const response = await userModel.findAll() 
+    const response = await userModel.findAll({
+      where: {
+        status: "AVAILABLE" 
+      }
+    }) 
     const data = response.filter((user) => user.dataValues) 
 
     res.status(200).json({
       success: true,
+      itemsFound: data.length,
       data 
     })
+
+    return
 
   } catch(err) {
 
@@ -24,25 +31,46 @@ async function GetUsers(req, res) {
       success: false,
       data: "something went wrong"
     })
+
   }
 
 }
 
-function GetSingleUser(req, res) {
+async function GetSingleUser(req, res) {
 
   const id = req.param("id")
-  
-  const sampleResponse = {
-    success: true,
-    data: {
-      id,
-      username: ""
-    }
-  }
 
-  res.status(200)
-  res.json(sampleResponse)
-}
+  try {
+    
+    const response = await userModel.findByPk(id, {}) 
+
+    if (response == null) { // NOT FOUND
+
+      res.status(404).json({
+        success: false,
+        data: "User not found"
+      }) 
+
+      return
+    }
+
+    const data = response.dataValues
+
+    res.status(200).json({
+      success: true,
+      data
+    })
+
+  } catch(err) {
+
+    console.log(err)
+
+    response.status(500).json({
+      success: false,
+      data: "something went wrong!"
+    })
+  }
+ }
 
 module.exports = {
   GetUsers,
